04d77e5e9376bf59771195d5071aba494c3b161b
Maksim Dzinisiuk <101088991+meowboom@users.noreply.github.com>
2024-06-29 17:56:34 +0200

    practice project

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,7 +1,21 @@
-import React, { FC } from "react";
+"use client";
+import React, { FC, useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { useInView } from "framer-motion";
 
 interface IReviews {}
+
+const ReviewGrid = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(containerRef, { once: true, amount: 0.4 });
+  //   RESUME
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    ></div>
+  );
+};
 
 const Reviews: FC<IReviews> = () => {
   return (
@@ -11,6 +25,7 @@ const Reviews: FC<IReviews> = () => {
         src="/what-people-are-buying.png"
         className="absolute select-none hidden xl:block -left-32 top-1/3"
       />
+      {/* <ReviewGrid/> */}
     </MaxWidthWrapper>
   );
 };
