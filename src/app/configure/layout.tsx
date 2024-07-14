77605adfbdf02dd177105e303e597ee24f4b6679
Maksim Dzinisiuk <101088991+meowboom@users.noreply.github.com>
2024-07-14 20:01:15 +0200

    procejt practice

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Steps from "@/components/Steps";
 import React, { FC, ReactNode } from "react";
 
 interface Props {
@@ -8,6 +9,7 @@ interface Props {
 const layout: FC<Props> = ({ children }) => {
   return (
     <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   );
